66e37b48d006cc030e28bc12781585c23d33cb5b
selfup <boudinot.regis@yahoo.com>
2018-03-23 20:51:00 -0500

    more notes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Example payload
+ * [{hello: 'world'}, {world: 'hello'}]
+ * will be actual JSON (aka stringified) in API Gateway
+ * Otherwise in lambda console it will be parsed JSON
+ */
+
 const handler = (event, context, callback) => {
   /**
    * if you end up using API Gateway
@@ -6,14 +13,22 @@ const handler = (event, context, callback) => {
    * event is already parsed
    */
   
+   // JSON.parse(event.body) is needed when using API Gateway
   const reqBody = event.body ? JSON.parse(event.body) : event;
 
   // this is not necessary but shows that you can do stuff anyways :P
-  const randomStuff = reqBody.map(e => Object.keys(e).map(e => e));
+  const payloadLength = reqBody
+    .map(e => Object.keys(e).map(e => e + ' '))
+    .length;
 
   // creating a body and statusCode is required for API Gateway
-  const body = JSON.stringify({ randomStuff: randomStuff.length });
-  const res = { statusCode: 200, body };
+  const body = JSON.stringify({ payloadLength });
+  const statusCode = 200;
+
+  const res = {
+    statusCode,
+    body,
+  };
 
   // here we pass the response to the callback :)
   callback(null, res);
